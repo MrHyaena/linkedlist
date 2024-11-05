23b2b56e8ccd1db7a6715d808473c36e06d5d149
MrHyaena <dolezalmartin131@gmail.com>
2024-11-05 18:56:27 +0100

    All the functions from excercise

--- src/index.js
+++ src/index.js
@@ -16,20 +16,6 @@ class node {
   }
 }
 
-// Test data
-
-const list = new LinkedList();
-append("one");
-append("two");
-append("three");
-prepend("four");
-
-size();
-head();
-tail();
-at(1);
-console.log(list);
-
 // Functions
 
 function append(value) {
@@ -93,3 +79,147 @@ function at(index) {
     console.log(current);
   }
 }
+
+function pop() {
+  if (list.head == null) {
+    console.log("List is empty");
+  } else {
+    let current = list.head;
+    let popCount = list.counter - 2;
+
+    for (let i = 0; i < popCount; i++) {
+      current = current.nextNode;
+    }
+
+    current.nextNode = null;
+    list.counter = list.counter - 1;
+    console.log(list);
+  }
+}
+
+function contains(value) {
+  if (list.head == null) {
+    console.log("List is empty");
+  } else {
+    let current = list.head;
+    let valueTrue = 0;
+
+    for (let i = 0; i < list.counter; i++) {
+      if (current.value == value) {
+        valueTrue = 1;
+      }
+      current = current.nextNode;
+    }
+
+    if (valueTrue == 1) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+}
+
+function find(value) {
+  if (list.head == null) {
+    console.log("List is empty");
+  } else {
+    let current = list.head;
+    let valueTrue = 0;
+    let indexValue = 0;
+
+    for (let i = 0; i < list.counter; i++) {
+      if (current.value == value) {
+        valueTrue = 1;
+        indexValue = i;
+      }
+      current = current.nextNode;
+    }
+
+    if (valueTrue == 1) {
+      console.log(indexValue);
+    } else {
+      console.log(null);
+    }
+  }
+}
+
+function toString() {
+  if (list.head == null) {
+  } else {
+    let current = list.head;
+    let string = "";
+
+    while (current.nextNode !== null) {
+      string = string + "( " + current.value + " ) -> ";
+      current = current.nextNode;
+    }
+
+    string = string + "( " + current.value + " )";
+    console.log(string);
+  }
+}
+
+function insertAt(value, index) {
+  let newNode = new node(value);
+  if (index > list.counter - 1) {
+    console.log("Index does not exist");
+  } else {
+    let current = list.head;
+
+    if (index == 0) {
+      list.head = null;
+      list.head = newNode;
+      list.head.nextNode = current;
+      list.counter = list.counter + 1;
+    } else {
+      for (let i = 0; i < index - 1; i++) {
+        current = current.nextNode;
+      }
+      console.log(current);
+      let memory = current.nextNode;
+      current.nextNode = null;
+      current.nextNode = newNode;
+      current.nextNode.nextNode = memory;
+      list.counter = list.counter + 1;
+    }
+  }
+  console.log(list);
+}
+
+function removeAt(index) {
+  if (index > list.counter - 1) {
+    console.log("Index does not exist");
+  } else {
+    let current = list.head;
+
+    if (index == 0) {
+      let memory = current.nextNode;
+      console.log(memory);
+      list.head = null;
+      list.head = memory;
+    } else {
+      for (let i = 0; i < index - 1; i++) {
+        current = current.nextNode;
+      }
+      let memory = current.nextNode.nextNode;
+      current.nextNode = null;
+      current.nextNode = memory;
+      console.log(memory);
+    }
+  }
+}
+
+// Test data
+
+const list = new LinkedList();
+append("one");
+append("two");
+append("three");
+prepend("four");
+
+contains("four");
+find("four");
+insertAt("dog", 0);
+toString();
+removeAt(4);
+toString();
